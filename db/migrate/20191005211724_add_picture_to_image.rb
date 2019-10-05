af2f4c6fc5e7ba8075f4ac46a24e9d1df7e37adb
Shanna Kershner <shanna.kershner@gmail.com>
2019-10-05 22:10:37 +0000

    db error update

--- db/migrate/20191005211724_add_picture_to_image.rb
+++
@@ -1,5 +0,0 @@
-class AddPictureToImage < ActiveRecord::Migration[5.2]
-  def change
-    add_column :images, :picture, :string
-  end
-end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_10_05_204418) do
+ActiveRecord::Schema.define(version: 2019_10_05_171145) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -24,11 +24,6 @@ ActiveRecord::Schema.define(version: 2019_10_05_204418) do
     t.datetime "updated_at", null: false
     t.string "image"
     t.index ["user_id"], name: "index_courses_on_user_id"
-  end
-
-  create_table "images", force: :cascade do |t|
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
   end
 
   create_table "lessons", force: :cascade do |t|
