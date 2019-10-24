e65c3451e1a9df9da144bcf1cf4a6f79823449dd
Shanna Kershner <shanna.kershner@gmail.com>
2019-10-24 00:33:15 +0000

    updated aws

--- config/initializers/carrierwave.rb
+++ config/initializers/carrierwave.rb
@@ -4,7 +4,6 @@ CarrierWave.configure do |config|
   config.storage    = :aws
   config.aws_bucket = ENV["AWS_BUCKET"]
   config.aws_acl    = "public-read"
-
   config.aws_credentials = {
     access_key_id:     ENV["AWS_ACCESS_KEY"],
     secret_access_key: ENV["AWS_SECRET_KEY"],
