9c7cb47d2cd6dae47674ee45d0d2f4c1874e2537
Shanna Kershner <shanna.kershner@gmail.com>
2019-10-25 02:48:23 +0000

    updated stylesheets

--- app/controllers/courses_controller.rb
+++ app/controllers/courses_controller.rb
@@ -6,4 +6,11 @@ class CoursesController < ApplicationController
   def show
     @course = Course.find(params[:id]) 
   end
+
+  private
+
+  helper_method :current_course
+  def current_course
+    @current_course ||= Course.find(params[:id])
+  end
 end
