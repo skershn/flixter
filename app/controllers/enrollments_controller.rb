bb9c0be32e14ceb65cd6480a432ba7cb875bd662
Shanna Kershner <shanna.kershner@gmail.com>
2019-10-28 06:44:02 +0000

    configured free and premium course enrollments

--- app/controllers/enrollments_controller.rb
+++ app/controllers/enrollments_controller.rb
@@ -2,22 +2,26 @@ class EnrollmentsController < ApplicationController
   before_action :authenticate_user!
 
   def create
-    current_user.enrollments.create(course: current_course)
+    #current_user.enrollments.create(course: current_course)
 
-    # Amount in cents
-    @amount = (current_course.cost * 100).to_i
+    if current_course.premium?
+      # Amount in cents
+      @amount = (current_course.cost * 100).to_i
 
-    customer = Stripe::Customer.create(
-      email: params[:stripeEmail],
-      source: params[:stripeToken]
-    )
+      customer = Stripe::Customer.create(
+        email: params[:stripeEmail],
+        source: params[:stripeToken]
+      )
 
-    charge = Stripe::Charge.create(
-      customer: customer.id,
-      amount: @amount,
-      description: 'Flixter Member Content',
-      currency: 'usd'
-    )
+      charge = Stripe::Charge.create(
+        customer: customer.id,
+        amount: @amount,
+        description: 'Flixter Member Content',
+        currency: 'usd'
+      )
+    end
+
+  current_user.enrollments.create(course: current_course)
   redirect_to course_path(current_course)
   rescue Stripe::CardError => e
     flash[:error] = e.message
